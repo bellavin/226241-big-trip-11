603f24721b7740b6668fd9762b3ba6fb033a8778
Dima Bellavin <izume@inbox.ru>
2020-04-06 23:12:01 +0300

    Разделил компоненты на модули

---
+++ src/components/trip-days.mjs
@@ -0,0 +1,28 @@
+import eventTmp from './event';
+import eventEditTmp from './event-edit';
+
+const tripDays = `<ul class="trip-days">
+  <li class="trip-days__item  day">
+    <div class="day__info">
+      <span class="day__counter">1</span>
+      <time class="day__date" datetime="2019-03-18">MAR 18</time>
+    </div>
+
+    <ul class="trip-events__list">
+      <li class="trip-events__item">
+        ${eventEditTmp}
+      </li>
+      <li class="trip-events__item">
+        ${eventTmp}
+      </li>
+      <li class="trip-events__item">
+        ${eventTmp}
+      </li>
+      <li class="trip-events__item">
+        ${eventTmp}
+      </li>
+    </ul>
+  </li>
+</ul>`;
+
+export default tripDays;
